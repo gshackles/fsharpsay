574a99eef8c6b853bf3a17ae6f1db1f6da02df44
Greg Shackles <gshackles@gmail.com>
2018-05-30 20:45:48 -0700

    add command

---
+++ Program.fs
@@ -0,0 +1,47 @@
+﻿let sayIt = printfn @"
+                            %s
+                            __________________
+                                              \
+                                               \
+                                                `  `                                                
+                                              `/+  /:`                                              
+                                            `/ss+  /++:`                                            
+                                          `/ssss+  /++++:.                                          
+                                        ./ssssss+  /++++++/.                                        
+                                      ./ssssssss+  /++++++++/.                                      
+                                    ./ssssssssss+  /++++++++++/.                                    
+                                  ./ssssssssssss+  +++++++++++++/-                                  
+                                ./ssssssssssssss/  /++++++++++++++/-`                               
+                              ./ssssssssssssss+.    ./+++++++++++++++-`                             
+                            ./ssssssssssssss+.        ./+++++++++++++++-`                           
+                          .+ssssssssssssss+.  `:+       ./+++++++++++++++:`                         
+                        .+ssssssssssssss+.  `:os+         ./+++++++++++++++:`                       
+                      .+ssssssssssssss+.  `:osss+           ./+++++++++++++++:`                     
+                    .+ssssssssssssss+.  `:osssss+             ./+++++++++++++++/.                   
+                  .+ssssssssssssss+.  `:osssssss+               ./+++++++++++++++/.                 
+                 /sssssssssssssso-   .osssssssss+                 -++++++++++++++++:                
+                  -+ssssssssssssso/`  `:osssssss+               `:+++++++++++++++/.                 
+                    -+ssssssssssssso/`  `:osssss+             `:+++++++++++++++/.                   
+                      -+ssssssssssssss/.  `:osss+           `:+++++++++++++++/.                     
+                        -+ssssssssssssss/.  `:os+         .:+++++++++++++++:.                       
+                          -+ssssssssssssss/.  `:+       ./+++++++++++++++:`                         
+                            .+ssssssssssssss/.  `     ./+++++++++++++++:`                           
+                              .+ssssssssssssss/.    ./+++++++++++++++:`                             
+                                .+ssssssssssssss/  :+++++++++++++++-`                               
+                                  .+ssssssssssss+  /+++++++++++++-`                                 
+                                    .+ssssssssss+  /++++++++++/-`                                   
+                                      .+ssssssss+  /++++++++/-                                      
+                                        .+ssssss+  /++++++/.                                        
+                                          .+ssss+  /++++/.                                          
+                                            .+ss+  /++/.                                            
+                                              .++  /:.                                              
+                                                `  `      "
+
+[<EntryPoint>]
+let main argv =
+    match argv with 
+        | [|message|] -> message
+        | _ -> "F# rocks!"
+    |> sayIt
+
+    0
